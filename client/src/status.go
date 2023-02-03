ab4e596a6cda9a2a99d0fcfe8c5b22369eed36e3
pasindu <pasindu.13@cse.mr.ac.lk>
2023-02-03 16:45:13 +0100

    added status.go for client

---
+++ client/src/status.go
@@ -0,0 +1,43 @@
+package src
+
+import (
+	"async-consensus/common"
+	"async-consensus/proto"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/*
+	When a status response is received, print it to console
+*/
+
+func (cl *Client) handleClientStatusResponse(response *proto.Status) {
+	fmt.Printf("status response %v\n", response)
+}
+
+/*
+	Send a status request to all the replicas
+*/
+
+func (cl *Client) SendStatus(operationType int) {
+	cl.debug("Sending status request to all replicas", 0)
+
+	for name, _ := range cl.replicaAddrList {
+
+		statusRequest := proto.Status{
+			Type:   int32(operationType),
+			Note:   "",
+			Sender: int64(cl.clientName),
+		}
+
+		rpcPair := common.RPCPair{
+			Code: cl.messageCodes.StatusRPC,
+			Obj:  &statusRequest,
+		}
+
+		cl.sendMessage(name, rpcPair)
+		cl.debug("Sent status to "+strconv.Itoa(int(name)), 0)
+	}
+	time.Sleep(time.Duration(statusTimeout) * time.Second)
+}
