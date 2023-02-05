3657ef8cec1899e21a63413b257c4ab45ae3ee51
pasindu <pasindu.13@cse.mr.ac.lk>
2023-02-05 12:19:24 +0100

    added paxos

---
+++ replica/src/paxossmr.go
@@ -0,0 +1,32 @@
+package src
+
+import (
+	"async-consensus/proto"
+	"fmt"
+	"strconv"
+	"time"
+)
+
+/*
+	update SMR logic
+*/
+
+func (rp *Replica) updatePaxosSMR() {
+
+	for i := rp.paxosConsensus.lastCommittedLogIndex + 1; i < int32(len(rp.paxosConsensus.replicatedLog)); i++ {
+
+		if rp.paxosConsensus.replicatedLog[i].decided == true {
+			var cllientResponses []*proto.ClientBatch
+			cllientResponses = rp.updateApplicationLogic(rp.paxosConsensus.replicatedLog[i].decisions.Requests)
+			if rp.paxosConsensus.replicatedLog[i].decisions.Sender == int64(rp.name) {
+				rp.sendClientResponses(cllientResponses)
+			}
+			rp.debug("Committed paxos consensus instance "+"."+strconv.Itoa(int(i))+" at time "+fmt.Sprintf("%v", time.Now().Sub(rp.paxosConsensus.startTime)), 5)
+			rp.paxosConsensus.lastCommittedLogIndex = i
+			rp.paxosConsensus.lastCommittedTime = time.Now()
+		} else {
+			break
+		}
+
+	}
+}
