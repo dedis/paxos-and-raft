e229a5197a7e267ba96bb9528155e852ade35d78
pasindu <pasindu.13@cse.mr.ac.lk>
2023-02-05 17:01:55 +0100

    checked crash-recovery-test and readme

---
+++ experiments/README.md
@@ -0,0 +1,5 @@
+This directory contains
+
+(1) bash scripts to run local tests
+
+(2) python scripts to check for correctness and simulate attacks
---
+++ experiments/python/crash-recovery-test.py
@@ -0,0 +1,55 @@
+import atexit
+import os
+import random
+import sys
+import time
+
+print("Starting crash recovery script")
+sys.stdout.flush()
+process_logs = [sys.argv[1], sys.argv[2], sys.argv[3]]
+wait_time = sys.argv[4]
+
+
+def find_process_ids(process_logs):
+    process_ids = []
+    for i in range(len(process_logs)):
+        lines = []
+        with open(process_logs[i]) as file:
+            lines = [line.rstrip() for line in file]
+        
+        line= ""
+        
+        for j in range(len(lines)):
+            if lines[j].startswith("--Initialized"):
+                line = lines[j]
+                break
+        
+        process_id = line.split(" ")[7]
+        process_ids.append(process_id)
+    return process_ids
+
+
+processes = find_process_ids(process_logs)
+
+print("The process IDs are " + str(processes))
+sys.stdout.flush()
+
+
+def exit_handler():
+    print("Ending!")
+    sys.stdout.flush()
+    os.system("kill -CONT " + processes[0])
+    os.system("kill -CONT " + processes[1])
+    os.system("kill -CONT " + processes[2])
+
+
+atexit.register(exit_handler)
+
+t_end = time.time() + 40
+while time.time() < t_end:
+    randomInstance = random.randint(0, 2)
+    print("stopping" + str(processes[randomInstance]))
+    os.system("kill -STOP " + processes[randomInstance])
+    time.sleep(int(wait_time))
+    os.system("kill -CONT " + processes[randomInstance])
+    time.sleep(1)
