62cccfe8f99e3e9265f5c11324812df7f37d5bde
Pasindu Tennage <pasindu.13@cse.mrt.ac.lk>
2023-02-17 23:21:45 +0100

    checked all files

--- experiments/bash/local/local_consensus_test.sh
+++ experiments/bash/local/local_consensus_test.sh
@@ -2,8 +2,9 @@
 arrivalRate=$1
 algo=$2
 viewTimeoutTime=$3
-attackTime=$4
-testTime=$5 # seconds 
+testTime=$4 # seconds
+batchTime=$5
+batchSize=$5
 
 replica_path="replica/bin/replica"
 ctl_path="client/bin/client"
@@ -17,9 +18,9 @@ pkill client; pkill client; pkill client
 
 echo "Killed previously running instances"
 
-nohup ./${replica_path} --name 1 --consAlgo "${algo}" --batchSize 50 --batchTime 1   --debugOn --debugLevel 7 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}1.log &
-nohup ./${replica_path} --name 2 --consAlgo "${algo}" --batchSize 50 --batchTime 1   --debugOn --debugLevel 7 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}2.log &
-nohup ./${replica_path} --name 3 --consAlgo "${algo}" --batchSize 50 --batchTime 1   --debugOn --debugLevel 7 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}3.log &
+nohup ./${replica_path} --name 1 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 27 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}1.log &
+nohup ./${replica_path} --name 2 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 27 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}2.log &
+nohup ./${replica_path} --name 3 --consAlgo "${algo}" --batchSize "${batchSize}" --batchTime "${batchTime}"   --debugOn --debugLevel 27 --viewTimeout "${viewTimeoutTime}" --pipelineLength 1 >${output_path}3.log &
 
 echo "Started 3 replicas"
 
@@ -39,9 +40,9 @@ echo "sent consensus start up status"
 
 echo "starting clients"
 
-nohup ./${ctl_path} --name 11 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize 50 --batchTime 1000 --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}11.log &
-nohup ./${ctl_path} --name 12 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize 50 --batchTime 1000 --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}12.log &
-./${ctl_path}       --name 13 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize 50 --batchTime 1000 --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}13.log
+nohup ./${ctl_path} --name 11 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}11.log &
+nohup ./${ctl_path} --name 12 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}12.log &
+./${ctl_path}       --name 13 --requestType request --defaultReplica 2  --debugOn --debugLevel 6 --batchSize  "${batchSize}" --batchTime "${batchTime}" --arrivalRate "${arrivalRate}" --leaderTimeout "${viewTimeoutTime}" --testDuration "${testTime}"  >${output_path}13.log
 
 sleep 10
 
@@ -60,5 +61,8 @@ pkill client; pkill client; pkill client
 python3 experiments/python/overlay-test.py "${testTime}" logs/1-consensus.txt logs/2-consensus.txt logs/3-consensus.txt > ${output_path}python-consensus.log
 
 echo "Killed instances"
+
+mkdir -p    /home/pasindu/Desktop/paxos_raft/"${arrivalRate}"/"${algo}"/"${viewTimeoutTime}"/"${testTime}"/"${batchTime}"/"${batchSize}"
+cp -r logs/ /home/pasindu/Desktop/paxos_raft/"${arrivalRate}"/"${algo}"/"${viewTimeoutTime}"/"${testTime}"/"${batchTime}"/"${batchSize}"
 
 echo "Finish test"
